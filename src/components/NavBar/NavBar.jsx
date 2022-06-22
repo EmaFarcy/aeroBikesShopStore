95b98d696f41efef17d742520d296e0f2caf425b
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-06-22 18:49:11 -0300

    contenido de la navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,27 @@
+import "./navbar.css";
+
 const NavBar= () => {
     return (
-        <div>
-            <p>Prueba</p>
+        <div class="container-fluid bike_container">
+		    <header>	
+			    <div id="menu">
+				    <nav class="nav1">
+					    	<a href="index.html"><img id="logo_aero" src="img/LogoAero.jpg" alt="aero bikes logo"></a>
+					    <ul>
+					    	<li><a href="">Home</a></li>
+					    	<li><a href="">About Us</a></li>
+					    	<li><a href="">Bike Shop</a></li>
+					    	<li><a href="">Accesories</a></li>
+					    	<li><a href="">Contact</a></li>
+					    	<li><a href="">LogIn</a></li>
+				    	</ul>
+				    </nav>
+				    <div class="fake-line"></div>
+			    </div>
+            </header>
         </div>
-    )
+            
+    );
 }
 
 export default NavBar;
