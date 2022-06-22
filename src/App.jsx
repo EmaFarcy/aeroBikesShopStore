7701cac04944f05f63dc15f6ad582f5116255246
Emanuel Farcy <emanuelfarcy@gmail.com>
2022-06-22 18:21:59 -0300

    Se agrega la carpeta components, la navbar y se crea un componente contenedor ItemListContainer

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <p>
+          Aero Bikes shop store (coming soon)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
